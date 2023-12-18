d32a6d5649c5064f5eb585aa33166663137e24b7
CAPPTA\arturo.americo <arturo.americo@cappta.com.br>
2023-12-18 09:47:06 -0300

    segunda adequação para SEO, ajuste de timeout para o loader

--- Alterado/buyer-file/plumber-demo/assets/js/functions.js
+++ Alterado/buyer-file/plumber-demo/assets/js/functions.js
@@ -252,7 +252,7 @@ return false;
 
 
 $(window).load(function() {
-    $("#loading").delay(2000).fadeOut(500);
+    $("#loading").delay(500).fadeOut(500);
     $("#loading-center").click(function() {
     $("#loading").fadeOut(500);
     })
